b62209ec181f75d0b915a8186d31c0562b1003c5
Tim Clark <timeeeee@gmail.com>
2023-05-27 08:30:28 -0400

    add project model

--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,18 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
+import enum
+
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, Text, Enum
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
 from database import Base
+
+# these should be pretty much static!
+class BucketEnum(enum.Enum):
+    trash = 0
+    maybe = 1
+    active = 2
+    complete = 3
+
 
 class InItem(Base):
     __tablename__ = "in_item"
@@ -12,7 +22,23 @@ class InItem(Base):
     created_at = Column(DateTime, server_default=func.Now())
     processed_at = Column(DateTime)
 
+    # when this is processed, the item could go in the trash/maybe, *or* it could become
+    # associated with a project (which in turn has a bucket)
     
+
+
+class Project(Base):
+    __tablename__ = "project"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    notes = Column(Text)
+    bucket = Column(Enum(BucketEnum), nullable=False)
+    next_step = Column(String(200))
+    created_at = Column(DateTime, server_default=func.Now())
+    updated_at = Column(DateTime, onupdate=func.Now())
+
+
 """
 class User(Base):
     __tablename__ = "users"
