8d06f07ea1123cdde4d37d0aced8803644bc3960
Tim Clark <timeeeee@gmail.com>
2023-05-26 20:34:28 -0400

    post new items

--- frontend/src/components/InItemList.js
+++ frontend/src/components/InItemList.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import axios from 'axios';
 
 import InItem from './InItem';
@@ -6,21 +6,59 @@ import InItem from './InItem';
 function InItemList() {
     const [isLoading, setIsLoading] = useState(true);
     const [inItems, setInItems] = useState([]);
+	const [isInputDisabled, setIsInputDisabled] = useState(false);
+	const [uploadError, setUploadError] = useState();
 
+	const formRef = useRef(null);
+	const descriptionInputRef = useRef(null);
+
+	// load the items on first render
     useEffect(() => {
-	axios.get('http://localhost:8000/in-items')
-	    .then(response => {
-		setIsLoading(false);
-		setInItems(response.data);
+		axios.get('http://localhost:8000/in-items')
+		.then(response => {
+			setIsLoading(false);
+			setInItems(response.data);
 	    });
     }, []);
+
+	// focus every render
+	useEffect(() => {
+		const element = descriptionInputRef.current;
+		if (element !== null) element.focus();
+	}, [inItems]);
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		setIsInputDisabled(true);
+		axios.post('http://localhost:8000/in-items', {'description': event.target.description.value})
+		.then(response => {
+			setIsInputDisabled(true);
+			setInItems([...inItems, response.data]);
+			formRef.current.reset();
+		})
+		.catch(error => {
+			setUploadError(error);
+		})
+		.finally(() => {
+			setIsInputDisabled(false);
+		});
+	}
 
     if (isLoading) return <p>loading...</p>;
     
     return (
-	<ul>
-	    {inItems.map(item => <InItem description={item.description} />)}
-	</ul>
+	<>
+		<ul>
+			{inItems.map(item => <InItem description={item.description} key={item.id} />)}
+		</ul>
+
+		<form onSubmit={handleSubmit} ref={formRef}>
+			<h2>Add an item</h2>
+			<input type='text' name='description' disabled={isInputDisabled} ref={descriptionInputRef} />
+			<input type='submit' disabled={isInputDisabled} value='submit' />
+			{(uploadError !== undefined) && <p>{uploadError}</p>}
+		</form>
+	</>
     );
 }
 
