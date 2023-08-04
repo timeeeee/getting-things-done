91c2fb71d59d829dd57d7bca6da4c2985ce42a59
Tim Clark <timeeeee@gmail.com>
2023-08-04 12:27:26 -0400

    add basic test

---
+++ backend/tests/test_end_to_end.py
@@ -0,0 +1,45 @@
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+
+from main import app, get_db
+from database import Base
+
+SQLALCHEMY_TEST_DATABASE_URL = "mysql://gtd_test:gtd_test@172.22.80.1/gtd_test"
+
+engine = create_engine(SQLALCHEMY_TEST_DATABASE_URL)
+SessionTest = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+"""
+def override_get_db():
+      try:
+          db = TestingSessionLocal()
+          yield db
+      finally:
+          db.close()
+"""
+
+def override_get_db():
+    connection = engine.connect()
+
+    # begin a non-ORM transaction
+    transaction = connection.begin()
+
+    # bind an individual Session to the connection
+    db = Session(bind=connection)
+
+    yield db
+
+    db.close()
+    transaction.rollback()
+    connection.close()
+
+app.dependency_overrides[get_db] = override_get_db
+client = TestClient(app)
+
+
+def test_pass():
+    response = client.get("/in-items/")
+    print(response)
