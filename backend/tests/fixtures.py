9bfb7246c5449d7db23ae379f7caa33f28fb95ba
Tim Clark <timeeeee@gmail.com>
2023-08-04 18:59:20 -0400

    add project fixtures

--- backend/tests/fixtures.py
+++ backend/tests/fixtures.py
@@ -1,11 +1,34 @@
-from crud import in_items
+import crud
 import schemas
 import models
 
 
 def add_test_data(db):
-    # add fixtures
-    in_items.create_in_item(db, schemas.InItemCreate(description="this is an in item"))
-    in_items.create_in_item(db, schemas.InItemCreate(description="this is another item"))
+    items = [
+        schemas.InItemCreate(description="this is an in item"),
+        schemas.InItemCreate(description="this is another item"),
+    ]
 
-    
+    for item in items:
+        crud.in_items.create_in_item(db, item)
+
+    projects = [
+        schemas.ProjectCreate(
+            name="Getting Things Done website",
+            notes="fastapi back-end, react front-end",
+            bucket=models.BucketEnum.active,
+            next_step="test"
+        ),
+        schemas.ProjectCreate(
+            name="Apply for jobs",
+            bucket=models.BucketEnum.active,
+            next_step="sign up on tutoring website"
+        ),
+        schemas.ProjectCreate(
+            name="The Contraption",
+            bucket=models.BucketEnum.complete
+        ),
+    ]
+
+    for project in projects:
+        crud.projects.create_project(db, project)
