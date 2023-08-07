d819d65ca1498f9ab126e2a60992e56232b4ddf7
Tim Clark <timeeeee@gmail.com>
2023-08-07 12:11:46 -0400

    edit projects and put the result to the API

--- frontend/src/components/Project.js
+++ frontend/src/components/Project.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import ReactMarkdown from 'react-markdown';
+
 
 function Project({projectId}) {
     const [data, setData] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [isEditing, setIsEditing] = useState(false);
 
     useEffect(() => {
 		axios.get(`http://localhost:8000/projects/${projectId}`)
@@ -13,17 +16,65 @@ function Project({projectId}) {
 	    });
     }, []);
 
+    const handleEdit = () => {
+        setIsEditing(true);
+    }
+
+    const handleSave = (event) => {
+        event.preventDefault();
+
+        setIsEditing(false);
+        setIsLoading(true);
+        
+        const putData = {
+            name: event.target.name.value,
+            notes: event.target.notes.value,
+            bucket: data.bucket,
+            next_step: event.target.next_step.value,
+        };
+
+        axios.put(`http://localhost:8000/projects/${projectId}`, putData)
+        .then(response => {
+            setIsLoading(false);
+            setData(response.data);
+        });
+    }
+
     if (isLoading) return <p>loading...</p>;
 
-    return (
-        <>
-            <h1>Project: {data.name}</h1>
-            <div>created {data.created_at}</div>
-            <div>status: {data.bucket}</div>
-            <div>next step: {data.next_step}</div>
-            <div>notes: {data.notes}</div>
-        </>
-    );
+    if (isEditing) {
+        return (
+             <form method='put' onSubmit={handleSave}>
+                <label>
+                    Name:
+                    <input type='text' name='name' defaultValue={data.name} />
+                </label>
+                
+                <label>
+                    Next step:
+                    <input type='text' name='next_step' defaultValue={data.next_step} />
+                </label>
+                
+                <label>
+                    Notes:
+                    <textarea name='notes' defaultValue={data.notes} />
+                </label>
+                
+                <button type='submit'>Save</button>
+            </form>
+        );
+    } else {
+        return (
+            <>
+                <h1>Project: {data.name}</h1>
+                <div>created {data.created_at}</div>
+                <div>status: {data.bucket}</div>
+                <div>next step: {data.next_step}</div>
+                <div>notes: <ReactMarkdown>{data.notes}</ReactMarkdown></div>
+                <button onClick={handleEdit}>edit</button>
+            </>
+        );
+    }
 }
 
 export default Project;
